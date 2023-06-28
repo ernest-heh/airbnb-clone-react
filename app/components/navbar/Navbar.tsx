3ae23e738fcaf5bd20105b13dc0587a0f0fb9e7b
Ernest Ho <ernest.heh@gmail.com>
2023-06-29 03:15:16 +0800

    display listing categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { FC } from "react";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -30,6 +31,7 @@ const Navbar: FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
