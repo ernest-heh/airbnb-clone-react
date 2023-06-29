951f4194d2280ed743e2797501a92a26d706784d
Ernest Ho <ernest.heh@gmail.com>
2023-06-29 11:28:28 +0800

    add leaflet map to step 2 of create listing form

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import CategoryInput from "../inputs/CategoryInput";
 import { CATEGORIES } from "../navbar/Categories";
 import Modal from "./Modal";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -45,6 +46,18 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
+
+  const Map = useMemo(
+    () =>
+      dynamic(
+        () =>
+          import("../Map", {
+            ssr: false,
+          })
+      ),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -108,7 +121,11 @@ const RentModal = () => {
           title="Where is your place located?"
           subtitle="Help guests find you!"
         />
-        <CountrySelect />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map center={location?.latlng} />
       </div>
     );
   }
