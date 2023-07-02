74cbd4f9b04ca008a2b69d3e029f6764039b7a58
Ernest Ho <ernest.heh@gmail.com>
2023-07-02 17:09:39 +0800

    import NextAuthOptions instead of AuthOptions

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import { authOptions } from "@/app/utils/authOptions";
+import { authOptions } from "@/app/libs/auth";
 import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-// export default NextAuth(authOptions);
--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -1,13 +1,13 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import bcrypt from "bcrypt";
-import { AuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 import prisma from "@/app/libs/prismadb";
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
